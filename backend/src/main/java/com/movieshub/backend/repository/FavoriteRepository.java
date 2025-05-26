a3e0a5f0fe764a80d1a4666253c04eb6fa195372
Dev Kumar <devkr074@gmail.com>
2025-05-26 19:59:14 +0530

    Initial Commit

--- backend/src/main/java/com/movieshub/backend/repository/FavoriteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.movieshub.backend.repository;
-
-import com.movieshub.backend.model.FavoriteMovie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface FavoriteRepository extends JpaRepository<FavoriteMovie, Long> {
-    List<FavoriteMovie> findByUserId(Long userId);
-}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import MovieSearch from "./components/MovieSearch";
-import Favorites from "./components/Favorites";
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/movies" element={<MovieSearch />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </Router>
-  );
+    <></>
+  )
 }
-
-export default App;
+export default App
