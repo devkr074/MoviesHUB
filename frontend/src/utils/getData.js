8b70af86b9a2ff1ff1230d7e4d72df548fc172ea
Dev Kumar <devkr074@gmail.com>
2025-05-28 22:12:07 +0530

    Created getData File

---
+++ frontend/src/utils/getData.js
@@ -0,0 +1,19 @@
+async function getData(url) {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                "trakt-api-key": "719bb35af98c7c9a1eb126dd97ffc070cb99202fd060c7b58e2f1148e03e3d3d",
+                "trakt-api-version": "2"
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+export default getData;
