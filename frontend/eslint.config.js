8759ecf0f9ef547498d0f53af212e442af624e11
Dev Kumar <devkr074@gmail.com>
2025-05-31 23:58:27 +0530

    Added Tailwinf CSS

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,7 +1,7 @@
-import js from "@eslint/js"
-import globals from "globals"
-import reactHooks from "eslint-plugin-react-hooks"
-import reactRefresh from "eslint-plugin-react-refresh"
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 export default [
   { ignores: ["dist"] },
   {
@@ -12,12 +12,12 @@ export default [
       parserOptions: {
         ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: "module",
-      },
+        sourceType: "module"
+      }
     },
     plugins: {
       "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      "react-refresh": reactRefresh
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -25,8 +25,8 @@ export default [
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
       "react-refresh/only-export-components": [
         "warn",
-        { allowConstantExport: true },
-      ],
-    },
-  },
-]
+        { allowConstantExport: true }
+      ]
+    }
+  }
+];
