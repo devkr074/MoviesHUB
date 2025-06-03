27fd8b60ef51100c6ed724e708fa77eddab53737
Dev Kumar <devkr074@gmail.com>
2025-06-03 18:11:28 +0530

    Project Setup

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to MovieDB</h1>
+      <p>Your personal movie collection</p>
+    </div>
+  );
+}
+
+export default Home;
