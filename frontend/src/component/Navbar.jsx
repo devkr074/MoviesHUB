7a5f9f7fca54f9b9a48671032acbe7242e9e2d4c
Dev Kumar <devkr074@gmail.com>
2025-06-06 23:01:51 +0530

    Updated Navbar Styling

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -5,32 +5,36 @@ function Navbar({ user, setUser }) {
     localStorage.removeItem("user");
     window.location.href = "/";
   }
+  function handleSearch() {
+    const searchCont=document.getElementById("sear");
+    searchCont.classList.toggle("top-height");
+
+  }
   return (
-    <div className="navbar d-flex align-items-center justify-content-between">
-      <div className="logo d-flex align-items-center justify-content-center gap-3 px-3">
+    <div className="navbar d-flex align-items-center justify-content-between px-4">
+      <div className="search-cont" id="sear">
+        <button onClick={handleSearch}>Close</button>
+      </div>
+      <div className="logo d-flex align-items-center justify-content-center gap-3">
         <a href="/"><img src={logo} alt="MoviesHUB Logo" height={50} /></a>
-        <div className="search-container">
-          <input type="text" />
-          <div className="search-result"></div>
-        </div>
-        <button><i class="fa-solid fa-magnifying-glass"></i></button>
+        <button className="search-btn" onClick={handleSearch}><i class="fa-solid fa-magnifying-glass"></i></button>
       </div>
       <div className="links d-flex align-items-center gap-4">
-        <div className="navbar-toggler position-relative">
-        <button><i class="fa-solid fa-bars"></i></button>
-        <div className="navbar-toggler-link shadow position-absolute end-0">
-          <ul>
-            <li><a href="/shows">Shows</a></li>
-            <li><a href="/movies">Movies</a></li>
-          </ul>
-        </div>
+        <div className="position-relative">
+          <button className="navbar-toggler"><i class="fa-solid fa-bars"></i></button>
+          <div className="navbar-toggler-link shadow position-absolute end-0">
+            <ul>
+              <li><a href="/shows">Shows</a></li>
+              <li><a href="/movies">Movies</a></li>
+            </ul>
+          </div>
         </div>
         <ul className="d-flex align-items-center gap-4">
           <li><a href="/shows">Shows</a></li>
           <li><a href="/movies">Movies</a></li>
         </ul>
-        <div className="user position-relative">
-          <button><i class="fa-solid fa-user"></i></button>
+        <div className="position-relative">
+          <button className="user"><i class="fa-solid fa-user"></i></button>
           <div className="user-link shadow position-absolute end-0">
             <ul>
               <li><a href="/login">Login</a></li>
