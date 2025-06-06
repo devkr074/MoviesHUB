22a6b764fb981c591d0e32c9a38e346a3674b927
Dev Kumar <devkr074@gmail.com>
2025-06-06 22:30:24 +0530

    Updated Navbar

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,3 +1,4 @@
+import logo from "../asset/logo.png"
 function Navbar({ user, setUser }) {
   function handleLogout() {
     setUser(null);
@@ -5,60 +6,40 @@ function Navbar({ user, setUser }) {
     window.location.href = "/";
   }
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <a className="navbar-brand" href="/">MoviesHUB</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="/shows">Shows</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/movies">Movies</a>
-            </li>
-          </ul>
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            {user ? (
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">{user.username}</a>
-                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="/profile">Profile</a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/favorites">Favorites</a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/library">Library</a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <button className="dropdown-item" onClick={handleLogout}>Logout</button>
-                  </li>
-                </ul>
-              </li>
-            ) : (
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Guest</a>
-                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="/login">Login</a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/signup">Sign Up</a>
-                  </li>
-                </ul>
-              </li>
-            )}
+    <div className="navbar d-flex align-items-center justify-content-between">
+      <div className="logo d-flex align-items-center justify-content-center gap-3 px-3">
+        <a href="/"><img src={logo} alt="MoviesHUB Logo" height={50} /></a>
+        <div className="search-container">
+          <input type="text" />
+          <div className="search-result"></div>
+        </div>
+        <button><i class="fa-solid fa-magnifying-glass"></i></button>
+      </div>
+      <div className="links d-flex align-items-center gap-4">
+        <div className="navbar-toggler position-relative">
+        <button><i class="fa-solid fa-bars"></i></button>
+        <div className="navbar-toggler-link shadow position-absolute end-0">
+          <ul>
+            <li><a href="/shows">Shows</a></li>
+            <li><a href="/movies">Movies</a></li>
           </ul>
         </div>
+        </div>
+        <ul className="d-flex align-items-center gap-4">
+          <li><a href="/shows">Shows</a></li>
+          <li><a href="/movies">Movies</a></li>
+        </ul>
+        <div className="user position-relative">
+          <button><i class="fa-solid fa-user"></i></button>
+          <div className="user-link shadow position-absolute end-0">
+            <ul>
+              <li><a href="/login">Login</a></li>
+              <li><a href="/signup">Sign Up</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </nav>
+    </div>
   );
 }
 export default Navbar;
