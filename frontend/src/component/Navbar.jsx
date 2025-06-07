0991f10a3af08b1874728ea7264a190fdc538ba7
Dev Kumar <devkr074@gmail.com>
2025-06-07 10:40:15 +0530

    Updated Navbar

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,4 +1,6 @@
-import logo from "../asset/logo.png"
+import { useEffect } from "react";
+import logo from "../asset/logo.png";
+
 function Navbar({ user, setUser }) {
   function handleLogout() {
     setUser(null);
@@ -6,107 +8,102 @@ function Navbar({ user, setUser }) {
     window.location.href = "/";
   }
 
-  const navbar = document.querySelector("[data-navbar]");
-  const navbarLinks = document.querySelectorAll("[data-nav-link]");
-  const menuToggleBtn = document.querySelector("[data-menu-toggle-btn]");
-  menuToggleBtn.addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    this.classList.toggle("active");
-  });
-  for (let i = 0; i < navbarLinks.length; i++) {
-    navbarLinks[i].addEventListener("click", function () {
-      navbar.classList.toggle("active");
-      menuToggleBtn.classList.toggle("active");
-    });
-  }
-  const header = document.querySelector("[data-header]");
-  const backTopBtn = document.querySelector("[data-back-top-btn]");
-  window.addEventListener("scroll", function () {
-    if (window.scrollY >= 100) {
-      header.classList.add("active");
-      backTopBtn.classList.add("active");
-    } else {
-      header.classList.remove("active");
-      backTopBtn.classList.remove("active");
+  useEffect(() => {
+    const navbar = document.querySelector("[data-navbar]");
+    const menuToggleBtn = document.querySelector("[data-menu-toggle-btn]");
+    const navbarLinks = document.querySelectorAll("[data-nav-link]");
+    const header = document.querySelector("[data-header]");
+    const backTopBtn = document.querySelector("[data-back-top-btn]");
+    const searchBtn = document.querySelector("[data-search-btn]");
+    const searchContainer = document.querySelector("[data-search-container]");
+    const searchSubmitBtn = document.querySelector("[data-search-submit-btn]");
+    const searchCloseBtn = document.querySelector("[data-search-close-btn]");
+    
+    if (menuToggleBtn) {
+      menuToggleBtn.addEventListener("click", function () {
+        navbar.classList.toggle("active");
+        this.classList.toggle("active");
+      });
     }
-  });
-  const searchBtn = document.querySelector("[data-search-btn]");
-  const searchContainer = document.querySelector("[data-search-container]");
-  const searchSubmitBtn = document.querySelector("[data-search-submit-btn]");
-  const searchCloseBtn = document.querySelector("[data-search-close-btn]");
-  const searchBoxElems = [searchBtn, searchSubmitBtn, searchCloseBtn];
-  for (let i = 0; i < searchBoxElems.length; i++) {
-    searchBoxElems[i].addEventListener("click", function () {
-      searchContainer.classList.toggle("active");
-      document.body.classList.toggle("active");
+
+    navbarLinks.forEach((link) => {
+      link.addEventListener("click", function () {
+        navbar.classList.toggle("active");
+        menuToggleBtn.classList.toggle("active");
+      });
     });
-  }
-  const deliveryBoy = document.querySelector("[data-delivery-boy]");
-  let deliveryBoyMove = -80;
-  let lastScrollPos = 0;
-  window.addEventListener("scroll", function () {
-    let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
-    if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
-      let activeScrollPos = window.scrollY;
-      if (lastScrollPos < activeScrollPos) {
-        deliveryBoyMove += 1;
+
+    window.addEventListener("scroll", function () {
+      if (window.scrollY >= 100) {
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
       } else {
-        deliveryBoyMove -= 1;
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active");
       }
-      lastScrollPos = activeScrollPos;
-      deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
-    }
-  });
+    });
 
+    [searchBtn, searchSubmitBtn, searchCloseBtn].forEach((elem) => {
+      elem.addEventListener("click", function () {
+        searchContainer.classList.toggle("active");
+        document.body.classList.toggle("active");
+      });
+    });
 
+  }, []);
 
   return (
     <>
-      <header class="header" data-header>
-        <div class="container">
+      <header className="header" data-header>
+        <div className="container">
           <h1>
-            <a href="#top" class="logo">Foodie<span class="span">.</span></a>
+            <a href="/" className="logo">
+            Movies<span className="span">HUB</span>
+            </a>
           </h1>
-          <nav class="navbar" data-navbar>
-            <ul class="navbar-list">
-              <li class="nav-item active">
-                <a href="index.html" class="navbar-link" data-nav-link>Home</a>
+          <nav className="navbar" data-navbar>
+            <ul className="navbar-list">
+              <li className="nav-item">
+                <a href="/shows" className="navbar-link" data-nav-link>
+                  Shows
+                </a>
               </li>
-              <li class="nav-item">
-                <a href="shop.html" class="navbar-link" data-nav-link>Shop</a>
-              </li>
-              <li class="nav-item">
-                <a href="about.html" class="navbar-link" data-nav-link>About Us</a>
-              </li>
-              <li class="nav-item">
-                <a href="contact.html" class="navbar-link" data-nav-link>Contact Us</a>
+              <li className="nav-item">
+                <a href="/movies" className="navbar-link" data-nav-link>
+                  Movies
+                </a>
               </li>
             </ul>
           </nav>
-          <div class="header-btn-group">
-            <button class="search-btn" aria-label="Search" data-search-btn>
-              <ion-icon name="search-outline"></ion-icon>
+          <div className="header-btn-group">
+            <button className="search-btn" aria-label="Search" data-search-btn>
+              <i class="fa-solid fa-magnifying-glass"></i>
             </button>
-            <button class="btn btn-hover"><a href="#booking">Reservation</a></button>
-            <button class="nav-toggle-btn" aria-label="Toggle Menu" data-menu-toggle-btn>
-              <span class="line top"></span>
-              <span class="line middle"></span>
-              <span class="line bottom"></span>
+            <button className="nav-toggle-btn" aria-label="Toggle Menu" data-menu-toggle-btn>
+              <span className="line top"></span>
+              <span className="line middle"></span>
+              <span className="line bottom"></span>
             </button>
           </div>
         </div>
       </header>
-      <div class="search-container" data-search-container>
-        <div class="search-box">
-          <input type="search" name="search" aria-label="Search here" placeholder="Type keywords here..."
-            class="search-input" />
-          <button class="search-submit" aria-label="Submit search" data-search-submit-btn>
-            <ion-icon name="search-outline"></ion-icon>
+      <div className="search-container" data-search-container>
+        <div className="search-box">
+          <input
+            type="search"
+            name="search"
+            aria-label="Search here"
+            placeholder="Type keywords here..."
+            className="search-input"
+          />
+          <button className="search-submit" aria-label="Submit search" data-search-submit-btn>
+            <i class="fa-solid fa-magnifying-glass"></i>
           </button>
         </div>
-        <button class="search-close-btn" aria-label="Cancel search" data-search-close-btn></button>
+        <button className="search-close-btn" aria-label="Cancel search" data-search-close-btn></button>
       </div>
     </>
   );
 }
+
 export default Navbar;
--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <div className="text-center">
+    <div className="text-center vh-100 bg-dark">
       <h1>MoviesHUB</h1>
       <p>Welcome to MoviesHUB! Explore your favorite movies and shows.</p>
     </div>
