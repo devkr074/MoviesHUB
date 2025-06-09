e990c212d329de14bd5a5b94573c0fe71eb7f61d
Dev Kumar <devkr074@gmail.com>
2025-06-09 19:06:44 +0530

    Updated Navbar

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,13 +1,42 @@
 import { useEffect } from "react";
-
 function Navbar({ user, setUser }) {
-  function openSearch(){
-    document.getElementById("search-container")
-  }
-  function addClass(){
-    document.getElementById("nav-toggle-btn").classList.toggle("active");
-    document.getElementById("navbar").classList.toggle("active");
-  }
+  useEffect(() => {
+    const searchBtn = document.querySelector("[data-search-btn]");
+    const searchContainer = document.querySelector("[data-search-container]");
+    const searchSubmitBtn = document.querySelector("[data-search-submit-btn]");
+    const searchCloseBtn = document.querySelector("[data-search-close-btn]");
+    const searchBoxElems = [searchBtn, searchSubmitBtn, searchCloseBtn];
+    for (let i = 0; i < searchBoxElems.length; i++) {
+      searchBoxElems[i].addEventListener("click", function () {
+        searchContainer.classList.toggle("active");
+        document.body.classList.toggle("active");
+      });
+    }
+    const navbar = document.querySelector("[data-navbar]");
+    const navbarLinks = document.querySelectorAll("[data-nav-link]");
+    const menuToggleBtn = document.querySelector("[data-menu-toggle-btn]");
+    menuToggleBtn.addEventListener("click", function () {
+      navbar.classList.toggle("active");
+      this.classList.toggle("active");
+    });
+    for (let i = 0; i < navbarLinks.length; i++) {
+      navbarLinks[i].addEventListener("click", function () {
+        navbar.classList.toggle("active");
+        menuToggleBtn.classList.toggle("active");
+      });
+    }
+    const header = document.querySelector("[data-header]");
+    const backTopBtn = document.querySelector("[data-back-top-btn]");
+    window.addEventListener("scroll", function () {
+      if (window.scrollY >= 100) {
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
+      } else {
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active");
+      }
+    });
+  }, []);
   return (
     <>
       <header className="header" data-header>
@@ -26,10 +55,10 @@ function Navbar({ user, setUser }) {
             </ul>
           </nav>
           <div className="header-btn-group">
-            <button onClick={openSearch} className="search-btn" aria-label="Search" data-search-btn>
+            <button className="search-btn" aria-label="Search" data-search-btn>
               <i class="fa-solid fa-magnifying-glass"></i>
             </button>
-            <button id="nav-toggle-btn" onClick={addClass} className="nav-toggle-btn" aria-label="Toggle Menu" data-menu-toggle-btn>
+            <button id="nav-toggle-btn" className="nav-toggle-btn" aria-label="Toggle Menu" data-menu-toggle-btn>
               <span className="line top"></span>
               <span className="line middle"></span>
               <span className="line bottom"></span>
@@ -37,7 +66,7 @@ function Navbar({ user, setUser }) {
           </div>
         </div>
       </header>
-      <div className="search-container" data-search-container>
+      <div className="search-container" id="search-container" data-search-container>
         <div className="search-box">
           <input type="search" name="search" aria-label="Search here" placeholder="Type keywords here..." className="search-input" />
           <button className="search-submit" aria-label="Submit search" data-search-submit-btn>
