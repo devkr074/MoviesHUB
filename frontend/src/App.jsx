941e98c064c15c6e2e940f855838038e9fc09a0b
Dev Kumar <devkr074@gmail.com>
2025-06-13 22:53:53 +0530

    Created Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,12 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 function App() {
   return (
-    <></>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 export default App;
