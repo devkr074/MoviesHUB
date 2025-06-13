ffb13159f08ea8890517ab128d1331cfe2261057
Dev Kumar <devkr074@gmail.com>
2025-06-13 23:09:05 +0530

    Updated Home Page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary position-fixed w-100">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">Navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import Header from "./components/Header.jsx";
+import Header from "../components/Header.jsx";
 function Home() {
     return (
         <>
             <Header />
-            <div>
-                <h1>Welcome to the Home Page</h1>
-                <p>This is the main page of our application.</p>
+            <div className="row m-0">
+                <div className="col-12 bg-dark text-white vh-100 d-flex flex-column justify-content-center align-items-center">
+                    <h1 className="text-center">MoviesHUB</h1>
+                    <p className="col-sm-12 col-md-8 col-lg-6 col-xl-6 col-xxl-6 text-center mt-3 fs-5">Explore. Discover. Share.</p>
+                </div>
             </div>
         </>
     );
