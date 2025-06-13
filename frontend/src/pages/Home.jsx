65c9c5e1ec93b39a67cf7bb9731e072a1ae3fdfb
Dev Kumar <devkr074@gmail.com>
2025-06-13 22:55:49 +0530

    Created Header Component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import Header from "./components/Header.jsx";
 function Home() {
     return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            <p>This is the main page of our application.</p>
-        </div>
+        <>
+            <Header />
+            <div>
+                <h1>Welcome to the Home Page</h1>
+                <p>This is the main page of our application.</p>
+            </div>
+        </>
     );
 }
 export default Home;
