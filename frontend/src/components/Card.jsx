299ec4ec83add5ca8ce273d5e12cda300a52245d
Dev Kumar <devkr074@gmail.com>
2025-06-15 00:23:40 +0530

    Updated Shows Page

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,10 +1,10 @@
-import placeholder from "../assets/placeholder.png";
+import placeholder from "../assets/placeholder.webp";
 function Card(props) {
   return (
-    <div className="ratio ratio-16x9 card position-relative shadow-sm" style={{ backgroundColor: "gray", backgroundImage: `url(${placeholder})`, backgroundSize: "60% 70%", backgroundRepeat: "no-repeat", backgroundPosition: "center", borderRadius: "8px" }}>
-      <img src={`https://${props.image}`} alt={title} className="lazy-load" style={{ opacity: 0, transition: "opacity 0.5s ease-in-out", transitionDelay: "0.2s" }} onLoad={(e) => (e.target.style.opacity = 1)} />
+    <div className="ratio ratio-16x9 card position-relative shadow-sm" style={{ backgroundImage: `url(${placeholder})`,overflow:"hidden", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", borderRadius: "8px" }}>
+      <img src={`https://${props.image}`} alt={props.title} className="lazy-load" style={{ opacity: 0, transition: "opacity 0.5s ease-in-out", transitionDelay: "0.2s" }} onLoad={(e) => (e.target.style.opacity = 1)} />
       <div className="card-body">
-        <p className="card-title position-absolute bottom-0 start-0 m-2 text-white fw-bold">{props.title}</p>
+        <p className="card-title position-absolute bottom-0 start-0 m-2 text-white fw-bold" style={{textShadow: "1px 1px 0px black,1px -1px 0px black,-1px 1px 0px black,-1px -1px 0px black"}} >{props.title}</p>
       </div>
     </div>
   );
--- frontend/src/pages/Shows.jsx
+++ frontend/src/pages/Shows.jsx
@@ -32,22 +32,22 @@ function Shows() {
         }
     };
     return (
-        <div className="container-fluid mt-4">
-            <div className="btn-group w-100 sticky-top bg-white py-2 shadow-sm">
-                {["trending", "popular", "anticipated"].map((index, c) => (
+        <div className="container-fluid mt-4 bg-dark min-vh-100">
+            <div className="w-100 sticky-top bg-white py-2 shadow-sm overflow-auto d-flex gap-2">
+                {["trending", "popular", "anticipated","streaming"].map((c,index) => (
                     <button key={index} className={`btn ${category == c ? "btn-dark" : "btn-outline-dark"}`} onClick={() => handleCategoryChange(c)}>{c.charAt(0).toUpperCase() + c.slice(1)}</button>
                 ))}
             </div>
             <h2 className="mb-3 text-center">{category.charAt(0).toUpperCase() + category.slice(1)} Shows</h2>
             {loading ? (
-                <div className="text-center mt-4">
+                <div className="text-center mt-4 min-vh-100 d-flex justify-content-center align-items-center">
                     <div className="spinner-border text-primary"></div>
                 </div>
             ) : (
                 <div className="row m-0">
-                    {shows.slice(0, visibleCount).map((index, s) => (
+                    {shows.slice(0, visibleCount).map((s,index) => (
                         <div key={index} className="col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 mb-3">
-                            <Card title={s.title || s.show.title} image={s.images.fanart || s.show.images.fanart || null} />
+                            <Card title={s.title || s.show.title} image={s.images?.fanart || s.show.images?.fanart || null} />
                         </div>
                     ))}
                 </div>
