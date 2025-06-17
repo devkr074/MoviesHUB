ba9fc816bc892f9474e61b7b9c965ca80de20f8b
Dev Kumar <devkr074@gmail.com>
2025-06-17 23:04:44 +0530

    Updated Home Banner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Header from "../components/Header.jsx";
 import Card from "../components/Card.jsx";
+import logo from "../assets/logo.png"
 const API_URL = import.meta.env.VITE_API_URL;
 const API_HEADERS = {
     "Content-Type": "application/json",
@@ -18,7 +19,6 @@ function Home() {
             const response = await fetch(`${API_URL}/shows/trending?limit=4&extended=full,images`, { headers: API_HEADERS });
             const data = await response.json();
             setShows(data);
-            setBackgroundImage(data[Math.floor(Math.random() * 4)]?.show?.images?.fanart || null);
         } catch (error) {
             console.error("Error fetching shows:", error);
         }
@@ -28,6 +28,7 @@ function Home() {
             const response = await fetch(`${API_URL}/movies/trending?limit=4&extended=full,images`, { headers: API_HEADERS });
             const data = await response.json();
             setMovies(data);
+            setBackgroundImage(data[Math.floor(Math.random() * 4)]?.movie?.images?.fanart || null);
         } catch (error) {
             console.error("Error fetching movies:", error);
         }
@@ -42,9 +43,12 @@ function Home() {
             <Header />
             <div className="row m-0">
                 <div style={{ backgroundImage: `url(https://${backgroundImage})`, backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }} className="vh-100 col-12 d-flex flex-column align-items-center justify-content-center bg-black position-relative">
-                    <div style={{ background: "-webkit-radial-gradient(center, circle cover, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.4) 100%)" }} className="col-12 position-absolute z-1 vh-100">
-                        <h1 className="text-light text-center position-absolute z-2">MoviesHUB</h1>
-                        <p className="col-sm-12 col-md-8 col-lg-6 col-xl-6 col-xxl-6 fs-5 text-light text-center mt-3">Explore. Discover. Track.</p>
+                    <div style={{ background: "-webkit-radial-gradient(center, circle cover, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.4) 100%)" }} className="col-12 position-absolute z-1 vh-100 d-flex flex-column align-items-center justify-content-center">
+                        <h1 className="mb-4 fw-bold text-light text-center d-flex align-items-center justify-content-center"><img src={logo} alt="" height={80} />MoviesHUB</h1>
+                        <h2 className="fw-bold text-light text-center">Discover. Track. Share.</h2>
+                        <div className="col-sm-12 col-md-8 col-lg-6 col-xl-6 col-xxl-6 text-center">
+                        <p className="px-2 text-center fs-5 text-light"><span className="fw-semibold">Discover</span> what's hot and where to stream it. <span className="fw-semibold">Track</span> shows and movies you watch. <span className="fw-semibold">Share</span> comments, recommendations, and ratings.</p>
+                        </div>
                     </div>
                 </div>
             </div>
