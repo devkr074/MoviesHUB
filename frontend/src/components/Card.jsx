059539563b430f8854a478b48125df0d8dd452b3
Dev Kumar <devkr074@gmail.com>
2025-06-22 23:45:48 +0530

    Updated Card Component

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,7 +1,18 @@
 import { BookmarkIcon, StarIcon, HeartIcon } from "@heroicons/react/24/solid";
 import placeholder from "../assets/placeholder.webp";
 import justWatchLogo from "../assets/justWatchLogo.svg";
+import { useEffect } from "react";
 function Card(props) {
+  useEffect(() => {
+    const dateString = props.release;
+    const dateObject = new Date(dateString);
+
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = dateObject.toLocaleDateString('en-US', options);
+
+    console.log(formattedDate);
+
+  }, [])
   return (
     <div className={`w-full aspect-16/9 ${props.category != "Home" && "border border-r-[#2D2D2D] border-b-[#2D2D2D]"} ${props.category == "Home" && "px-3 my-3"} md:w-1/2 ${props.category == "Home" ? "lg:w-1/4" : "lg:w-1/3"}`}>
       <a href={`/${props.type}/${props.slug}`}>
@@ -10,18 +21,20 @@ function Card(props) {
             <img src={props.image} alt={props.title} className="h-full" style={{ opacity: 0, transition: "opacity 0.5s ease-in-out", transitionDelay: "0.5s" }} onLoad={(e) => (e.target.style.opacity = 1)} />}
           <div style={{ backgroundImage: "linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgb(0, 0, 0) 100%)" }} className="w-full px-3 py-2 absolute bottom-0">
             {props.category == "Trending" &&
-              <span className="h-6 bg-[#9F42C6] px-2 py-1 text-white text-xs rounded font-semibold">{props.watchers} Watchers</span>
+              <span className="h-6 bg-[#9F42C6] px-2 py-1 text-white text-xs rounded font-semibold">{props.watchers.toLocaleString('en-US')} Watchers</span>
             }
 
             {props.category == "Anticipated" &&
-              <span className="h-6 bg-[#0082CE] px-2 py-1 text-white text-xs rounded font-semibold">{props.waiting} Anticipated</span>
+              <>
+                <span className="h-6 bg-[#0082CE] px-2 py-1 text-white text-xs rounded font-semibold">{props.waiting.toLocaleString()} Anticipated</span>
+              </>
             }
             {props.category == "Streaming" &&
               <div className="flex gap-1">
                 <img src={justWatchLogo} className="h-6" alt="" />
                 <span className="h-6 bg-[#E1B702] px-2 py-1 text-xs rounded font-semibold">{props.rank}</span>
-                <span className="h-6 bg-black border border-[#FF0000] border-[#47AD45] rounded-2xl text-[#FF0000] px-2 py-1 text-xs rounded font-semibold">{props.delta > 0 ? <><i class="fa-solid fa-caret-up"></i>+{props.delta}</> : <><i class="fa-solid fa-caret-down"></i>{props.delta}</>}</span>
-              </div>
+                {props.delta && <span className={`bg-black border ${props.delta > 0 ? "border-[#47AD45] text-[#47AD45]" : "border-[#FF0000] text-[#FF0000]"}  rounded-xl px-2 text-xs rounded font-semibold flex items-center gap-1`}>{props.delta > 0 ? <><i class="fa-solid fa-caret-up mt-[4px]"></i>+{props.delta}</> : <><i class="fa-solid fa-caret-down"></i>{props.delta}</>}</span>
+                }</div>
             }
 
             <p className="w-full text-lg text-white font-semibold leading-none pt-2">{props.title} <span className="text-sm text-[#CCCCCC] font-normal">{props.year}</span></p>
